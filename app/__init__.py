7823b08e66513b70599d5a40af466aeccfd19237
albright.human <sungutialbright@gmail.com>
2022-05-01 12:54:59 +0300

    connect the instance folder to app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
-# from flask import Flask
+from flask import Flask
+from .config import DevConfig
 
-# if __name__ == '__main__':
-#     app = Flask(__name__)
+if __name__ == '__main__':
+    app = Flask(__name__)
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
