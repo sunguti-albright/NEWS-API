1cb7d7d5f096b543c16fcdf9b8d43deeaec2d449
albright.human <sungutialbright@gmail.com>
2022-05-03 17:19:53 +0300

    install and import flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
-if __name__ == '__main__':
-    app = Flask(__name__)
+#app initialization
+app = Flask(__name__, instance_relative_config=True)
+
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+bootstrap = Bootstrap(app)
+
 from app import views
+from app import error
