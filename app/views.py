cbe7992556a0b391f5f7b23cd85b7864bcd2e3a6
albright.human <sungutialbright@gmail.com>
2022-05-03 17:46:25 +0300

    add routes for homepage and articles page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from flask import render_template
 from app import app
+from .request import get_sources, get_article
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    source = get_sources()
+    title = "Home! Welcome to NewsSnooze"
+    return render_template('index.html', title=title, sources=source)
+
+@app.route('/article/<id>')
+def article(id):
+    
+    
+    articles = get_article(id)
+    return render_template('article.html', articles=articles,id=id )
+    
