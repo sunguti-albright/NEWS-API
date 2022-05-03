c0049d807f9662cc3fe9f67519fea326662216c4
albright.human <sungutialbright@gmail.com>
2022-05-03 17:03:09 +0300

    add news sources model class

---
+++ app/models/sources.py
@@ -0,0 +1,12 @@
+
+class Source:
+    """
+    Source class to define source objects
+    """
+
+    def __init__(self, id, title, description, url, category):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.url = url
+        self.category = category
