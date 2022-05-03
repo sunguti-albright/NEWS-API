b624af5f3be9d9cb7c185a99dc377227699292f9
albright.human <sungutialbright@gmail.com>
2022-05-03 03:11:28 +0300

    create news sources and articles class

---
+++ app/models/articles.py
@@ -0,0 +1,11 @@
+class Article:
+    '''
+    Article class to define Article Objects
+    '''
+    def __init__(self,title, description, image, time, url):
+        self.title = title
+        self.description = description
+        self.image = image
+        self.time = time
+        self.url = url
+        
